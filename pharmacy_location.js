339e236c2d320fbb6face220fa4e0f6fa6e33da9
Abdullatif88 <33044365+Abdullatif88@users.noreply.github.com>
2024-02-17 12:18:38 +0300

    Update pharmacy_location.js

--- pharmacy_location.js
+++ pharmacy_location.js
@@ -2,9 +2,8 @@ import {csv} from "https://cdn.skypack.dev/d3-fetch@3";
 
 const map = L.map('map').setView([29.25, 48], 9);
 
-const tiles = L.tileLayer('http://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+const tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 let circleGroup = L.layerGroup([]).addTo(map);
